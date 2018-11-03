919439d9d03c2bf42fe7ef9c59d633d55b4d7557
ZPlayzMCPE <jameshayward3800@gmail.com>
2018-11-03 19:49:11 +0000

    Updated

--- src/LDX/TimeCommander/TimeCommand.php
+++ src/LDX/TimeCommander/TimeCommand.php
@@ -1,13 +1,12 @@
 <?php
 namespace LDX\TimeCommander;
-use pocketmine\scheduler\PluginTask;
+use pocketmine\scheduler\Task;
 use pocketmine\Server;
-class TimeCommand extends PluginTask {
-  public function __construct($plugin,$cmd) {
+class TimeCommand extends Task {
+  public function __construct(Main $plugin, $cmd) {
     $this->plugin = $plugin;
     $this->cmd = $cmd;
     $this->start = false;
-    parent::__construct($plugin);
   }
   public function onRun(int $ticks) {
     if($this->start) {
